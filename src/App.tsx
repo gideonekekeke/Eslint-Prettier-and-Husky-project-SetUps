cb2f7b670e4bd4e6da812e9ca7ff881635163928
gideonekekeke <gideonekeke64@gmail.com>
2023-05-24 15:32:30 +0100

    husky config file set up completely

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,18 @@ import './App.css';
 
 function App() {
     const [count, setCount] = useState(0);
-    console.log('added to test');
+
+    // console.log('this would throw an error if you try to push');
 
     return (
         <>
             <div></div>
             <h1>Vite + React</h1>
             <div className="card">
-                
-				
-				
-				
-				     <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button>
+
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
