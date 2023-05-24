47f8e73a6f4454c6b4d651a25d66f52e5cb4f6e8
gideonekekeke <gideonekeke64@gmail.com>
2023-05-24 15:29:42 +0100

    husky config file set up completely

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,11 @@ function App() {
             <div></div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
+                
+				
+				
+				
+				     <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button>
                 <p>
